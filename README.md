938616d4167a56546ff8684a5624d0592dcbd190
Lucas Immerman <lucasimmerman@gmail.com>
2022-05-02 20:22:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Arc-io "Miner"
+
+### https://arc.io is A new way to make money. Arc is a peer-to-peer Content Delivery Network (CDN). Add Arc's widget to your website, your users share bandwidth, we rent it, and you get paid the proceeds. 
+
+
+So I thought I'd use a package called [puppeteer](https://www.npmjs.com/package/puppeteer).
+
+It lets you control google chrome, and in this case the code goes to a page with arc, thus you can earn money.
+
+Then the next problem was how do you host it? 
+
+**I used my vps [(GalaxyGate)](https://galaxygate.net)**
+
+### In the end you can earn up to 1.00$ every 3 days
---
+++ index.js
@@ -0,0 +1,38 @@
+const delay = require('delay');
+const puppeteer = require('puppeteer');
+const prompt = require("prompt-sync")({ sigint: true });
+const n = prompt("How Many Tab(s) Would You Like To Open? ");
+
+(async function example() {
+
+  while(true){
+    try {
+      let i = 0;
+
+      process.setMaxListeners(Infinity);
+      let browsers = []
+      while(i < n){
+
+      await delay(3000);
+      let browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+      
+      const page = await browser.newPage();
+      await browsers.push(browser)
+      //await browsers.push(browser)
+      //await page.setDefaultNavigationTimeout(Infinity); 
+      await page.goto('https://arcio-server.losh531.repl.co/');
+      let x = i + 1;
+      await console.log("Opening Tab Number " + x);
+      i++;
+    }
+    await delay(60000 * 1)
+    browsers.forEach(async function(item, index){
+      await item.close()
+    })
+  } finally {
+    await console.log("Finished Opening + waiting")
+    
+
+  }
+}
+})();
